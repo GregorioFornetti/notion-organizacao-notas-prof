dd6589d685352641ca6ab03cc11e90018449a69a
GregorioFornetti <63006135+GregorioFornetti@users.noreply.github.com>
2023-09-14 00:19:20 -0300

    coletando acess_token

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,18 @@
 
 import express from 'express'
 import 'dotenv/config'
+import fetch from 'node-fetch'
 
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import cors from "cors"
 
 const __filename = fileURLToPath(import.meta.url);
 const root =  dirname(dirname(__filename));
 
 const app = express()
 const port = 3000
+app.use(express.json(), cors())
 
 const dist_path = 'frontend/notion-organizador-notas/dist'
 
@@ -26,6 +29,26 @@ app.get('/notion-organizador-notas/app.html', (req, res) => {
 
 app.get('/notion-organizador-notas/assets/:file', (req, res) => {
   res.sendFile(`${dist_path}/notion-organizador-notas/assets/${req.params.file}`, {root: root})
+})
+
+app.post('/notion-organizador-notas/auth', (req, res) => {
+  const auth_response = fetch('https://api.notion.com/v1/oauth/token', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Basic ${Buffer.from(`${client_id}:${client_secret}`).toString('base64')}`
+    },
+    body: JSON.stringify({
+      grant_type: 'authorization_code',
+      code: req.body.code,
+      redirect_uri: process.env.VITE_REDIRECT_URI
+    })
+  })
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data)
+  })
+  console.log(req.body.code)
 })
 
 app.listen(port, () => {
--- frontend/notion-organizador-notas/src/App.vue
+++ frontend/notion-organizador-notas/src/App.vue
@@ -1,6 +1,18 @@
 
 <script setup lang="ts">
+  let params = new URLSearchParams(document.location.search)
+  let code = params.get("code")
 
+  
+  fetch(`${import.meta.env.VITE_SERVER_ROOT}/auth`, {
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    method: 'POST',
+    body: JSON.stringify({
+      code: code
+    })
+  })
 </script>
 
 <template>
